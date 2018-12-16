65396d2af71b6727f7a2512ee18f607e6f366e9a
yunpeng <liyunpeng0601@163.com>
2018-12-17 02:38:17 +0800

    yunpeng- first

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -50,14 +50,14 @@ class Order extends Base
         $data = input('param.');
         $validate = validate('IDMustBePostiveInt');
         if(!$validate->check($data)){
-            $this->result(null,0,$validate->getError());
+            return $this->result(null,0,$validate->getError());
         }
         $order = new OrderService();
         $success = $order->delivery($id);
         if(is_array($success) && $success['code'] == 0){
-            $this->result(null,$success['code'],$success['msg']);
+            return $this->result(null,$success['code'],$success['msg']);
         }else{
-            $this->result(null,1,'success');
+            return $this->result(null,1,'success');
         }
     }
 }
